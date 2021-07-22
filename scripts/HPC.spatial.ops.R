d07b294f644812b529d1656890610bb4acd4a60d
gakindler <42635017+gakindler@users.noreply.github.com>
2021-07-22 16:03:26 +1000

    Script paintball - in the name of trying to fix invalid geometries

--- scripts/HPC.spatial.ops.R
+++ scripts/HPC.spatial.ops.R
@@ -8,13 +8,37 @@ library(sf)
 #### Loading and subsetting ####
 
 electorates <- st_read("/QRISdata/Q4107/raw_data/AEC_2019_superseded/COM_ELB_region.shp")
+electorates.arc <- st_read("/QRISdata/Q4107/raw_data/electorates_arcGIS_repair/electorates_arcGIS_repair.shp")
 species <-  st_read("/QRISdata/Q4107/raw_data/SNES_public_1july2021.gdb")
 
-electorates <- select(electorates, -c("Elect_div", "State", "Area_SqKm", "geometry"))
+species.arc <-  st_read("/QRISdata/Q4107/raw_data/SNES_public_1july2021_arcGIS_repair/SNES_public_1july2021_arcGIS_repair.shp")
+
+electorates <- select(electorates, c("Elect_div", "State", "Area_SqKm", "geometry"))
+electorates_arc <- select(electorates, c("Elect_div", "State", "Area_SqKm", "geometry"))
 
 # Make valid? Wtf does this mean? https://r-spatial.org/r/2017/03/19/invalid.html 
-species <- st_make_valid(species)
 electorates <- st_make_valid(electorates)
+electorates.arc <- st_make_valid(electorates)
+species <- st_make_valid(species)
+
+species.b <- st_buffer(species[!is.na(valid)], 0.0)
+species.b <- st_buffer(species.public, 0.0)
+
+species.invalid <- st_make_valid(species.invalid)
+table((st_is_valid(species)))
+species.public <- st_make_valid(species.public)
+table((st_is_valid(species.public)))
+st_is_valid(species.public, reason = TRUE)
+
+species.invalid <- slice(species.public, 1946, 3206, 3247)
+table((st_is_valid(species.invalid)))
+species.invalid <- species.invalid %>% select(geometry)
+st_write(species.invalid, dsn = "analysed_data/invalid.geom.gpkg")
+
+
+[1213] Loop 167 is not valid: Edge 2591 crosses edge 2595
+[1365] Loop 0 edge 0 crosses loop 256 edge 4
+[2294] Loop 0 edge 3590 crosses loop 219 edge 0.
 
 #### Join intersect ####
 
--- scripts/spatial.joins.r
+++ scripts/spatial.joins.r
@@ -5,12 +5,13 @@
 
 library(tidyverse)
 library(sf)
-library(tmap)
 
 #### Loading and subsetting ####
 
 electorates <- st_read("raw_data/AEC_2019_superseded/COM_ELB_region.shp")
-species <-  st_read("raw_data/SNES_public_1july2021.gdb")
+species <- st_read("raw_data/SNES_public_1july2021.gdb")
+species.public <- st_read("raw_data/snes_public_grids_08Aug2019.gdb", layer = "species_combined")
+st_layers("raw_data/snes_public_grids_08Aug2019.gdb")
 # outline <- st_read("raw_data/aus_outline_nsaasr9nnd_02211a04es_geo/aust_cd66states.shp")
 
 # Check CRS's are the same
@@ -19,7 +20,7 @@ st_crs(electorates) == st_crs(species)
 species.sl <- slice_sample(species, n = 500) %>%
   select(c("SCIENTIFIC_NAME", "VERNACULAR_NAME", "THREATENED_STATUS",
            "Shape_Area", "Shape"))
-electorates.ss <- select(electorates, -c("Numccds", "Actual", "Projected", 
+electorates.ss <- select(electorates, -c("Numccds", "Actual", "Projected",
                                          "Total_Popu", "Australian", "Sortname"))
 
 # Make valid? Wtf does this mean? https://r-spatial.org/r/2017/03/19/invalid.html 
@@ -39,48 +40,48 @@ join.intersect <- st_join(electorates, species,
 
 ## Count species within each electorate ##
 # Add a column of no of species per electorate
-elect.spec.uniq.spec <- intersect %>% 
+elect.spec.uniq.spec <- join.intersect %>% 
   as_tibble() %>% 
   group_by(Elect_div) %>% 
   mutate(total_unique_spec = n_distinct(SCIENTIFIC_NAME))
 
 # Isolated table of no of species per electorate
-elect.spec.uniq.spec.tbl <- intersect %>% 
+elect.spec.uniq.spec.tbl <- join.intersect %>% 
   as_tibble() %>% 
   group_by(Elect_div) %>% 
   summarise(total_unique_spec = n_distinct(SCIENTIFIC_NAME))
 
 ## Count electorates on no of species within them ##
-
-elect.spec.uniq.elect <- intersect %>% 
+# Add a column
+elect.spec.uniq.elect <- join.intersect %>% 
   as_tibble() %>% 
   group_by(SCIENTIFIC_NAME) %>% 
   mutate(total_unique_elect = n_distinct(Elect_div))
 
 # Isolated table of no of species per electorate
-elect.spec.uniq.elect.tbl <- intersect %>% 
+elect.spec.uniq.elect.tbl <- join.intersect %>% 
   as_tibble() %>% 
   group_by(SCIENTIFIC_NAME) %>% 
   summarise(total_unique_elect = n_distinct(Elect_div))
 
 #### Join antijoin ####
 
-outside <- sapply(st_intersects(species.sl, electorates.ss), function(x){
-  length(x) == 0
-  })
-
-inside.elects <- lengths(st_intersects(electorates.ss, species.sl)) > 0
-outside.elects <- !inside.elects
+# outside <- sapply(st_intersects(species.sl, electorates.ss), function(x){
+#   length(x) == 0
+#   })
+# 
+# inside.elects <- lengths(st_intersects(electorates.ss, species.sl)) > 0
+# outside.elects <- !inside.elects
 
 #### Join difference ####
 
-join.diff <- st_difference(electorates.ss, species.sl)
-join.diff.swap <- st_difference(species.sl, electorates.ss)
+# join.diff <- st_difference(electorates.ss, species.sl)
+# join.diff.swap <- st_difference(species.sl, electorates.ss)
 
 #### Join intersect-ion ####
 
 # Intersection join, functions as an inner join
-intersection <- st_intersection(electorates.ss, species.sl)
+intersection <- st_intersection(electorates, species)
 
 # Make valid again?
 intersection <- st_make_valid(intersection)
@@ -100,33 +101,33 @@ intersection.calcarea <- intersection %>%
   summarise(area = sum(area))
 
 
-st_geometry(intersection.calcarea) <- NULL
-
-area.nogeom <- area
-intersection <- area.nogeom$AREA_HA == area.nogeom$area
-unique(intersection)
-
-#### Other ####
-
-st_geometry(intersect) <- NULL
-
-species.sl <- st_buffer(species.sl, 0.0)
-electorates.ss <- st_buffer(electorates.ss, 0.0)
-
-species.sl <- st_buffer(species.sl[!is.na(valid)], 0.0)
-electorates.ss <- st_buffer(electorates.ss[!is.na(valid)], 0.0)
-
-filter <- st_filter(electorates.ss, species.sl, .pred = st_intersects())
-
-#### Plotting ####
-
-tm.elect.spec <- tm_shape(elect.spec) +
-  tm_polygons(col = "lightblue")
-
-#### Saving ####
-
-write.csv(elect.spec.ng, file = "analysed_data/elect_spec.csv")
-
-tmap_save(tm.elect.spec, filename = "plots/tm_elect_spec.png", 
-          width = 600, height = 600)
+# st_geometry(intersection.calcarea) <- NULL
+# 
+# area.nogeom <- area
+# intersection <- area.nogeom$AREA_HA == area.nogeom$area
+# unique(intersection)
+# 
+# #### Other ####
+# 
+# st_geometry(intersect) <- NULL
+# 
+# species.sl <- st_buffer(species, 0.0)
+# electorates.ss <- st_buffer(electorates.ss, 0.0)
+# 
+# species.sl <- st_buffer(species.sl[!is.na(valid)], 0.0)
+# electorates.ss <- st_buffer(electorates.ss[!is.na(valid)], 0.0)
+# 
+# filter <- st_filter(electorates.ss, species.sl, .pred = st_intersects())
+# 
+# #### Plotting ####
+# 
+# tm.elect.spec <- tm_shape(elect.spec) +
+#   tm_polygons(col = "lightblue")
+# 
+# #### Saving ####
+# 
+# write.csv(elect.spec.ng, file = "analysed_data/elect_spec.csv")
+# 
+# tmap_save(tm.elect.spec, filename = "plots/tm_elect_spec.png", 
+#           width = 600, height = 600)
 
