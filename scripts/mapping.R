e40b657779ecc4fec19f47282beda0f954b48613
gakindler <42635017+gakindler@users.noreply.github.com>
2021-07-30 16:29:15 +1000

    Refining those mappies

--- scripts/mapping.R
+++ scripts/mapping.R
@@ -4,15 +4,54 @@ library(sf)
 library(tmap)
 library(leaflet)
 library(ggplot2)
+library(viridis)
 
 print(object.size(spec.per.elect.aus), units = "Kb")
 
 tm_shape(spec.per.elect.aus) +
-  tm_polygons("total_unique_spec") +
+  tm_polygons("total_unique_spec", 
+              style = "jenks", 
+              title = "Number of species",
+              palette = "-viridis") +
   tm_text("Elect_div", size = "AREA")
 
+# Get rid of lines?
+
+
+#### Leaflet mapping ####
+
+# Change CRS to what Leaflet likes
+spec.per.elect.aus.WGS84 <- st_transform(spec.per.elect.aus, crs = )
+st_crs(spec.per.elect.aus)
+
+# Mapping
+bins <- c(0, 25, 50, 75, 100, 125, 150, 175, 200, 225, 250, Inf)
+pal <- colorBin("YlOrRd", domain = spec.per.elect.aus$total_unique_spec, bins = bins)
+labels <- sprintf(
+  "<strong>Electorate</strong><br/>%s<br/>
+  <strong>Number of species</strong><br/>%d",
+  spec.per.elect.aus$Elect_div, spec.per.elect.aus$total_unique_spec
+  ) %>% lapply(htmltools::HTML)
+
+
 leaflet(spec.per.elect.aus) %>% 
-  addPolygons(fillColor = "total_unique_spec")
+  addPolygons(fillColor = ~pal(total_unique_spec), 
+              weight = 2,
+              opacity = 1,
+              color = "white",
+              dashArray = "3",
+              fillOpacity = 0.7,
+              highlight = highlightOptions(
+                weight = 2,
+                color = "#666",
+                dashArray = "",
+                fillOpacity = 0.7,
+                bringToFront = TRUE),
+              label = labels,
+              labelOptions = labelOptions(
+                style = list("font-weight" = "normal", padding = "3px 8px"),
+                textsize = "15px",
+                direction = "auto"))
   
-  
+
 
--- scripts/spatial.ops.R
+++ scripts/spatial.ops.R
@@ -36,32 +36,43 @@ australia.ss <- australia %>%
   select(geometry) %>% 
   slice(1)
 
-# Make valid
+# # Make valid
 species.ss <- st_make_valid(species.ss)
 electorates.ss <- st_make_valid(electorates.ss)
 
 # Simplify geometry
-species.ss <- st_simplify(species.ss, dTolerance = 25000) # 10000 metres?
-electorates.ss <- ms_simplify(electorates.ss, keep = 0.001, keep_shape = TRUE) # 1% of original?
-australia.ss <- ms_simplify(australia.ss, keep = 0.001, keep_shape = TRUE) %>% 
+species.ss <- st_simplify(species.ss, dTolerance = 5000) # ___ metres?
+electorates.ss <- ms_simplify(electorates.ss, keep = 0.1, keep_shape = TRUE) # __% of original?
+australia.ss <- ms_simplify(australia.ss, keep = 0.1, keep_shape = TRUE) %>%
   select(geometry)
 
 species.ss <- st_make_valid(species.ss)
 electorates.ss <- st_make_valid(electorates.ss)
+australia.ss <- st_make_valid(australia.ss)
 
 #### Join intersect ####
 
 # 'Electorates' object is the object x as we want to keep this geometry and
 # inner join as we only want the intersect, not any disjointed values
-join.intersect <- st_join(electorates.ss, species.ss, 
-                     join = st_intersects, 
+join.intersect <- st_join(electorates.ss, species.ss,
+                     join = st_intersects,
                      left = FALSE)
+
+new.durack <- join.intersect %>% 
+  filter(Elect_div == "Durack")
+st_geometry(new.durack) <- NULL
+new.durack <- new.durack %>% 
+  distinct(Elect_div, SCIENTIFIC_NAME, .keep_all = TRUE) %>% 
+  select(SCIENTIFIC_NAME, VERNACULAR_NAME)
+old.durack <- read.csv("analysed_data/old_durack.csv")
+old.durack <- rename(old.durack, SCIENTIFIC_NAME = Scientific.Name, VERNACULAR_NAME = Common.Name)
+join.durack <- anti_join(new.durack, old.durack, by = "SCIENTIFIC_NAME")
 
 ## Count species within each electorate ##
 # Add a column of no of species per electorate
-elect.spec.uniq.spec <- join.intersect %>% 
-  group_by(Elect_div) %>% 
-  mutate(total_unique_spec = n_distinct(SCIENTIFIC_NAME))
+# elect.spec.uniq.spec <- join.intersect %>% 
+#   group_by(Elect_div) %>% 
+#   mutate(total_unique_spec = n_distinct(SCIENTIFIC_NAME))
 
 # Aggregate per electorate while maintaining species list
 # elect.spec.uniq.spec.exp <- join.intersect %>% 
@@ -80,6 +91,10 @@ spec.per.elect <- electorates.ss %>%
 
 spec.per.elect.aus <- st_intersection(australia.ss, spec.per.elect) %>% 
   st_make_valid()
+
+spec.per.elect.aus.exp <- st_join(australia.ss, spec.per.elect,
+                          join = st_intersects,
+                          left = FALSE)
 
 ## Count electorates on no of species within them ##
 # Add a column
