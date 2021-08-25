1e4db9d9fa1fff980ca5eb3f0f2d9a733ff89a97
gakindler <42635017+gakindler@users.noreply.github.com>
2021-08-25 17:00:06 +1000

    Rejoin problem solved

--- scripts/spatial.ops.R
+++ scripts/spatial.ops.R
@@ -1,11 +1,10 @@
-# Back end spatial operations to answer the research questions
+# Back end spatial operations to answer research questions
 
 #### Libraries ####
 
 library(tidyverse)
 library(sf)
-library(rmapshaper)
-# library(remotes)
+library(rmapshaper) # For installing use 'library(remotes)'
 
 #### Loading and pre-processing ####
 
@@ -16,7 +15,7 @@ species <- st_read("raw_data/SNES_public_1july2021.gdb")
 australia <- st_read("raw_data/ASGS_Edition_3_Aust_2021_shapefile/AUS_2021_AUST_GDA94.shp")
 demography <- readxl::read_xlsx("raw_data/AEC_demographic-classification-1-january-2019/01-demographic-classification-as-at-1-january-2019.xlsx")
 
-# What are the unique values of each attribute?
+# # What are the unique values of each attribute?
 # unique <- lapply(species, unique)
 
 # Check CRS's are the same
@@ -47,22 +46,22 @@ aus.ss <- australia %>%
   select(geometry) %>% 
   slice(1)
 
-# Simplify geometry
-specs.ss <- st_simplify(specs.ss, 
-                        dTolerance = 20000) %>% # units of metres
-  st_make_valid() 
-elects.ss <- ms_simplify(elects.ss, 
-                         keep = 0.01, 
-                         keep_shape = TRUE) %>%  # __% of original? 
-  st_make_valid()
-aus.ss <- ms_simplify(aus.ss,
-                      keep = 0.01, 
-                      keep_shape = TRUE) %>%
-  select(geometry) %>% 
-  st_make_valid()
+# # Simplify geometry
+# specs.ss <- st_simplify(specs.ss,
+#                         dTolerance = 20000) %>% # units of metres
+#   st_make_valid()
+# elects.ss <- ms_simplify(elects.ss,
+#                          keep = 0.01,
+#                          keep_shape = TRUE) %>%  # __% of original?
+#   st_make_valid()
+# aus.ss <- ms_simplify(aus.ss,
+#                       keep = 0.01,
+#                       keep_shape = TRUE) %>%
+#   select(geometry) %>%
+#   st_make_valid()
 
 #### spec.per.elect - no. of specs per electorate and concentration ####
-# Count no. of specs per electorate, then snap onto Aus
+
 spec.per.elect <- elects.ss %>% 
   st_join(specs.ss) %>% 
   group_by(Elect_div) %>% 
@@ -81,7 +80,7 @@ spec.per.elect.aus <- st_intersection(aus.ss, spec.per.elect) %>%
 #   
 # exp <- split(specs.sl)
 
-#### demo.spec - How do species relate to AEC's demography? ####
+#### demo.spec - demography and species ####
 
 demo.spec <- elects.ss %>% 
   st_join(specs.ss) %>% 
@@ -94,8 +93,6 @@ demo.spec.aus <- st_intersection(aus.ss, demo.spec) %>%
 #### spec.range.elect - specs range within each electorate ####
 # Calculate total area of each species's range
 specs.ss.area <- specs.ss %>% 
-  filter(REGIONS == "WA") %>% 
-  # slice_sample(n = 100) %>% 
   mutate(spec_area_sqm = st_area(.) %>% as.numeric())
 
 # Calculate the percentage of species area within each electorate 
@@ -109,64 +106,26 @@ spec.range.elect.eighty <- spec.range.elect %>%
   filter(percent_range_within >= 0.8) %>% 
   select(-c("State", "Shape_Area", "spec_area_sqm", "Area_SqKm",
              "intersection_area_sqm", "Demographic_class")) %>% 
-  rename(Elect_div_orig = Elect_div)
+  st_set_geometry(NULL) %>% 
+  inner_join(elects.ss, by = c("Elect_div" = "Elect_div")) %>% 
+  st_as_sf() %>% 
+  group_by(Elect_div) %>% 
+  summarise(total_unique_spec = n_distinct(SCIENTIFIC_NAME))
 
-spec.range.elect.eighty.exp <- elects.ss %>% 
-  st_join(spec.range.elect.eighty, 
-          join = st_overlaps, 
-          left = FALSE)
-
-spec.range.elect.eighty.exp <- st_join(elects.ss, spec.range.elect.eighty, left = FALSE) 
-
-st_write(spec.range.elect.eighty, dsn = "analysed_data/spec_range_elect_eighty.shp", 
-         layer = 'spec.range.elect.eighty')
-
-table(spec.range.elect.eighty.exp$SCIENTIFIC_NAME)
-
-discrep.exp <- spec.range.elect.eighty.exp %>% 
-  group_by(SCIENTIFIC_NAME) %>% 
-  filter(n() > 1)
-  
-discrep.exp <- table(spec.range.elect.eighty.exp$SCIENTIFIC_NAME)
-
-discrep <- anti_join(spec.range.elect.eighty.exp, spec.range.elect.eighty,
-                     by = c("Elect_div" = "Elect_div_orig", "SCIENTIFIC_NAME" = "SCIENTIFIC_NAME"))
-
-st_write(discrep, dsn = "analysed_data/discrep/discrep.shp", 
-         layer = 'discrep')
-
-# PROBLEM: this join is duplicating rows that shouldn't need to be duplicated?
-# ANSWER: I did not understand how the intersects predicate works. 
-# If you've got geometry that shares the same boundaries i.e. federal electoral boundaries
-# And you try and join previously intersection-ed data with that geometry it is
-# going to share the boundaries with the geometry that was sliced it
-# Therefore if you try to rejoin with this geometry, it's not going to function 
-# how I wanted it to which was only capture where the majority of the geometry was contained
-# It's going to be captured by the shared borders too, therefore duplicating it
-# WHATTA GENIUS
-
-if (nrow(spec.range.elect.eighty) == nrow(spec.range.elect.eighty.exp)){
-  spec.range.elect.eighty <- spec.range.elect.eighty %>% 
-    group_by(Elect_div) %>% 
-    summarise(total_unique_spec = n_distinct(SCIENTIFIC_NAME))
-  spec.range.elect.eighty.aus <- st_intersection(aus.ss, spec.eighty.elect.exp) %>%
-    st_make_valid()
-} else{
-  print("This spatial join has duplicated rows")
-}
+spec.range.elect.eighty.aus <- st_intersection(aus.ss, spec.range.elect.eighty) %>%
+  st_make_valid()
 
 #### spec.endemic.elect - specs endemic to each electorate ####
-# Count no. of endemic specs per electorate, then snap onto Aus
 
-# TO DO: Add conditional evaluation to this section
-
+# Filter for species which are endemic to each electorate
 spec.endemic.elect <- spec.range.elect %>% 
   filter(percent_range_within == 1) %>% 
-  select(-c("Elect_div", "State", "Shape_Area"))
-
-spec.endemic.elect <- elects.ss %>% 
-  st_join(spec.endemic.elect, left = FALSE) %>% 
-  group_by(Elect_div) %>%
+  select(-c("State", "Shape_Area", "spec_area_sqm", "Area_SqKm",
+            "intersection_area_sqm", "Demographic_class")) %>% 
+  st_set_geometry(NULL) %>% 
+  inner_join(elects.ss, by = c("Elect_div" = "Elect_div")) %>% 
+  st_as_sf() %>% 
+  group_by(Elect_div) %>% 
   summarise(total_unique_spec = n_distinct(SCIENTIFIC_NAME))
 spec.endemic.elect.aus <- st_intersection(aus.ss, spec.endemic.elect) %>%
   st_make_valid()
@@ -184,47 +143,25 @@ elect.spec.cover.status <- specs.ss %>%
   select(c("SCIENTIFIC_NAME", "THREATENED_STATUS")) %>% 
   left_join(elect.spec.cover, by = "SCIENTIFIC_NAME") %>% 
   as.data.frame() %>% 
-  select(-"geometry") %>% 
-  drop_na()
+  select(-"geometry")
 
-#### spec.outside.elect - Clipping/antijoin ####
-# Couple of methods here:
-## 1. Same procedure as endemic but with st_difference
-specs.ss.area <- specs.ss %>% 
-  mutate(spec_area_sqm = st_area(Shape) %>% as.numeric())
+#### spec.outside.elect ####
+# Two methods here:
+## 1. Same procedure as species.range but with st_difference, allows the distinguishing
+## of how much of the species's range is outside electorates
+elects.ss.union <- st_union(elects.ss, by_feature = FALSE) %>% 
+  st_sf()
 
-spec.outside.elect <- st_difference(specs.ss.area, elects.ss) %>% 
+spec.outside.elect <- st_difference(specs.ss.area, elects.ss.union) %>%
   st_make_valid() %>% 
-  mutate(intersection_area_sqm = st_area(Shape) %>% as.numeric()) %>% 
-  transform(percent_range_within = intersection_area_sqm / spec_area_sqm)
-
-st_geometry(spec.outside.elect) <- NULL
-ggplot(spec.outside.elect, aes(x = percent_range_within)) +
-  geom_histogram()
-summary(spec.outside.elect$percent_range_within)
-
-# PROBLEM: Same as spec.range
-
-spec.range.elect.eighty <- spec.range.elect %>% 
-  filter(percent_range_within >= .8) %>% # Their range of less than 20% instead maybe? 
-  select(-c("State", "Shape_Area")) %>% 
-  rename(Elect_div_orig = Elect_div)
-
-spec.range.elect.eighty.exp <- elects.ss %>% 
-  st_join(spec.range.elect.eighty, left = FALSE)
-
-table(spec.range.elect.eighty$Elect_div == spec.range.elect.eighty$Elect_div_orig)
-
-group_by(Elect_div) %>%
-  summarise(total_unique_spec = n_distinct(SCIENTIFIC_NAME))
-spec.range.elect.eighty.aus <- st_intersection(aus.ss, spec.range.elect.eighty) %>%
-  st_make_valid()
+  mutate(outside_area_sqm = st_area(.) %>% as.numeric()) %>% 
+  mutate(percent_range_outside = outside_area_sqm / spec_area_sqm) %>% 
+  mutate(across(percent_range_outside, round, digits = 2)) %>% 
+  filter(percent_range_outside >= .8) # 80% who chose this? Did you? Did I?
 
 ## 2. Logical vector method
 # More from Ryan Peek - https://ryanpeek.org/mapping-in-R-workshop/03_spatial_joins.html
 # Make all electorates into the same feature
-elects.ss.union <- st_union(elects.ss, by_feature = FALSE) %>% 
-  st_sf()
 outside <- sapply(st_intersects(specs.ss, elects.ss.union), function(x){
   length(x) == 0
   })
@@ -232,17 +169,3 @@ spec.out <- specs.ss[outside, ]
 
 # Fact check
 antijoin.fc <- unique(join.intersect["SCIENTIFIC_NAME"])
-
-
-# #### Other ####
-# 
-# st_geometry(intersect) <- NULL
-# 
-# specs.sl <- st_buffer(specs, 0.0)
-# elects.ss <- st_buffer(elects.ss, 0.0)
-# 
-# specs.sl <- st_buffer(specs.sl[!is.na(valid)], 0.0)
-# elects.ss <- st_buffer(elects.ss[!is.na(valid)], 0.0)
-# 
-# filter <- st_filter(elects.ss, specs.sl, .pred = st_intersects())
-# 
