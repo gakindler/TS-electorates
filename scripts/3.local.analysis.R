8447cbb3d561d284602790000728a11f5468d580
gareth_kindler_SEES_pop_os <42635017+gakindler@users.noreply.github.com>
2021-12-15 17:52:55 +1000

    ensuring export to local analysis folder

--- scripts/3.local.analysis.R
+++ scripts/3.local.analysis.R
@@ -39,62 +39,100 @@ pop$Elect_div <- gsub("Mcmahon", "McMahon", pop$Elect_div)
 pop$Elect_div <- gsub("Mcpherson", "McPherson", pop$Elect_div)
 pop$Elect_div <- gsub("O'connor", "O'Connor", pop$Elect_div)
 
+demo.pop <- demo %>%
+  inner_join(pop)
+
 #### Import and analyse - HPC data ####
 
 #### spec.per.elect - no. of species per electorates, demography, and concentration ####
 
-spec.per.elect <- st_read(dsn = "analysed_data/21-12-06_HPC_spatial_ops_output/spec.per.elect.gpkg")
+spec.per.elect <- st_read(
+  dsn = "analysed_data/21-12-06_HPC_spatial_ops_output/spec.per.elect.gpkg"
+)
 spec.per.elect <- spec.per.elect %>%
-  inner_join(demo) %>%
-  inner_join(pop) %>%
+  inner_join(demo.pop) %>%
+  mutate(Elect_div_abbrev = substr(Elect_div, start = 1, stop = 2)) %>%
   relocate(
-    Elect_div, Demographic_class, Electors, total_unique_spec,
-    elect_area_sqkm, species_per_sqkm, geom
-  ) %>%
-  mutate(Elect_div_abbrev = substr(Elect_div, start = 1, stop = 2)) %T>%
+    Elect_div, Elect_div_abbrev, Demographic_class,
+    Electors, elect_area_sqkm, total_unique_spec,
+    species_per_sqkm, geom
+  ) %T>%
   st_write(
     dsn = "analysed_data/21-12-06_local_analysis_output/spec.per.elect.gpkg",
     layer = "spec.per.elect", append = FALSE
   ) %>%
   st_set_geometry(NULL) %T>%
-  write.csv("analysed_data/21-12-06_local_analysis_output/spec.per.elect.csv")
+  write.csv(
+    "analysed_data/21-12-06_local_analysis_output/spec.per.elect.csv",
+    row.names = FALSE
+  )
 
-#### spec.per.elect - nested no. of species per electorate ####
+#### spec.per.elect.indiv - indivdual species per electorate ####
 
-# spec.per.elect.nest <- st_read(dsn = "analysed_data/21-12-06_HPC_spatial_ops_output/spec.per.elect.gpkg")
-# spec.per.elect.nest <- spec.per.elect.nest %>%
-#   st_set_geometry(NULL) %>%
-#   inner_join(demo) %>%
-#   select(c("Elect_div", "total_unique_spec", ""))
-#   nest_by(Elect_div, State, Demographic_class, total_unique_spec) %T>%
-#   write_json(path = "analysed_data/21-12-06_local_analysis_output/spec.per.elect.nest.json")
+spec.per.elect.indiv <- fromJSON(
+  dsn = "analysed_data/21-12-06_HPC_spatial_ops_output/spec.per.elect.indiv.json"
+)
+spec.per.elect.indiv <- spec.per.elect.indiv %>%
+  inner_join(demo.pop) %>%
+  select(c("Elect_div", "total_unique_spec", "")) %>%
+  nest_by(Elect_div, State, Demographic_class, total_unique_spec) %T>%
+  write.csv(
+    path = "analysed_data/21-12-06_local_analysis_output/spec.per.elect.indiv.csv"
+  )
 
-#### spec.range.elect - species range within each electorate ####
-spec.eighty.elect <- st_read(dsn = "analysed_data/21-12-06_HPC_spatial_ops_output/spec.eighty.elect.gpkg")
+#### spec.endemic.elect/spec.eighty.elect - species range completely/80% within each electorate ####
 
-spec.eighty.elect.aus <- st_read(dsn = "analysed_data/21-12-06_HPC_spatial_ops_output/spec.eighty.elect.aus.gpkg")
+spec.endemic.elect <- st_read(
+  dsn = "analysed_data/21-12-06_HPC_spatial_ops_output/spec.endemic.elect.gpkg"
+) %>%
+  inner_join(demo.pop) %>%
+  rename(total_endemic_unique_spec = total_unique_spec) %T>%
+  st_write(
+    dsn = "analysed_data/21-12-06_local_analysis_output/spec.endemic.elect.gpkg",
+    layer = "spec.endemic.elect", append = FALSE
+  ) %>%
+  st_set_geometry(NULL)
 
+spec.eighty.elect <- st_read(
+  dsn = "analysed_data/21-12-06_HPC_spatial_ops_output/spec.eighty.elect.gpkg"
+) %>%
+  inner_join(demo.pop) %>%
+  rename(total_eighty_unique_spec = total_unique_spec) %T>%
+  st_write(
+    dsn = "analysed_data/21-12-06_local_analysis_output/spec.eighty.elect.gpkg",
+    layer = "spec.eighty.elect", append = FALSE
+  ) %>%
+  st_set_geometry(NULL)
 
-#### spec.eighty.elect - species range >80% in each electorate ####
-
-spec.endemic.elect <- st_read(dsn = "analysed_data/21-12-06_HPC_spatial_ops_output/spec.endemic.elect.gpkg")
-
-spec.endemic.elect.aus <- st_read(dsn = "analysed_data/21-12-06_HPC_spatial_ops_output/spec.endemic.elect.aus.gpkg")
-
-# TO DO: Count no. of endemic species per electorate in wide format?
+spec.endemic.eighty.elect <- spec.endemic.elect %>%
+  full_join(spec.eighty.elect) %>%
+  relocate(
+    Elect_div, Demographic_class, Electors,
+    total_endemic_unique_spec, total_eighty_unique_spec
+  ) %T>%
+  write.csv(
+    "analysed_data/21-12-06_local_analysis_output/spec.endemic.eighty.elect.csv",
+    row.names = FALSE
+  )
 
 #### elect.spec.cover - How many electorates does each species's range cover? ####
 
-elect.spec.cover <- read.csv(
-  "analysed_data/21-12-06_HPC_spatial_ops_output/elect.spec.cover.csv"
+elect.spec.cover <- fromJSON(
+  "analysed_data/21-12-06_HPC_spatial_ops_output/elect.spec.cover.json"
 )
 
-table(elect.spec.cover$elect_range_covers, useNA = "ifany")
-table(elect.spec.cover$MIGRATORY_STATUS, useNA = "ifany")
+elect.spec.cover <- elect.spec.cover %>%
+  relocate()
 
-elect.spec.cover$elect_range_covers <-
-  elect.spec.cover$elect_range_covers %>%
-  replace_na(0)
+write.csv(
+  elect.spec.cover,
+  "analysed_data/21-12-06_local_analysis_output/elect.spec.cover.csv",
+  row.names = FALSE
+)
+
+# elect.spec.cover$elect_range_covers <-
+#   elect.spec.cover$elect_range_covers %>%
+#   replace_na(0)
 
 #### spec.eighty.outside.elect ####
 
@@ -102,6 +140,18 @@ spec.outside.elect <- st_read(
   dsn = "analysed_data/21-12-06_HPC_spatial_ops_output/spec.outside.elect.gpkg"
 )
 
+write.csv(
+  spec.outside.elect,
+  "analysed_data/21-12-06_local_analysis_output/spec.outside.elect.csv",
+  row.names = FALSE
+)
+
 spec.outside.elect.logical <- st_read(
   dsn = "analysed_data/21-12-06_HPC_spatial_ops_output/spec.outside.elect.logical.gpkg"
+)
+
+write.csv(
+  spec.outside.elect.logical,
+  "analysed_data/21-12-06_local_analysis_output/spec.outside.elect.logical.csv",
+  row.names = FALSE
 )
