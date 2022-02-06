c8b9745c2d03347dcf4616b1f767a6c2be282a3f
gareth_kindler_SEES_pop_os <42635017+gakindler@users.noreply.github.com>
2022-02-06 16:34:32 +1000

    species generalised spatial problem attempt

--- scripts/1.HPC.clean.data.R
+++ scripts/1.HPC.clean.data.R
@@ -22,212 +22,253 @@ library(units)
 aus <- st_read("/QRISdata/Q4107/TS_electorates/raw_data/ASGS_Edition_3_Aust_2021_shapefile/AUS_2021_AUST_GDA94.shp")
 
 aus.clean <- aus %>%
-        select(geometry) %>%
-        slice(1) %>%
-        st_crop(
-                xmin = 112.921114, ymin = -43.740510,
-                xmax = 153.638727, ymax = -9.115517
-                # remove external islands
-        ) %T>%
-        st_write(
-                dsn = "/QRISdata/Q4107/TS_electorates/clean_data/aus.clean.gpkg",
-                layer = "aus.clean", append = FALSE, delete_dsn = TRUE
-        )
+  select(geometry) %>%
+  slice(1) %>%
+  st_crop(
+    xmin = 112.921114, ymin = -43.740510,
+    xmax = 153.638727, ymax = -9.115517
+    # remove external islands
+  ) %T>%
+  st_write(
+    dsn = "/QRISdata/Q4107/TS_electorates/clean_data/aus.clean.gpkg",
+    layer = "aus.clean", append = FALSE, delete_dsn = TRUE
+  )
 
 aus.union.clean <- aus.clean %>%
-        st_union(by_feature = FALSE) %>%
-        st_sf() %>%
-        st_make_valid() %T>%
-        st_write(
-                dsn = "/QRISdata/Q4107/TS_electorates/clean_data/aus.union.clean.gpkg",
-                layer = "aus.union.clean", append = FALSE, delete_dsn = TRUE
-        )
+  st_union(by_feature = FALSE) %>%
+  st_sf() %>%
+  st_make_valid() %T>%
+  st_write(
+    dsn = "/QRISdata/Q4107/TS_electorates/clean_data/aus.union.clean.gpkg",
+    layer = "aus.union.clean", append = FALSE, delete_dsn = TRUE
+  )
 
 #### Import & clean: Electorates ####
 
 elect <- st_read("/QRISdata/Q4107/TS_electorates/raw_data/2021-Cwlth_electoral_boundaries_ESRI/2021_ELB_region.shp")
 
 elect.clean <- elect %>%
-        select(
-                Elect_div, geometry
-        ) %>%
-        rename(
-                electorate = Elect_div
-        ) %>%
-        st_make_valid() %>%
-        st_crop(
-                xmin = 111.921114, ymin = -44.740510,
-                xmax = 154.638727, ymax = -8.115517
-                # remove the islands
-        ) %>%
-        mutate(
-                electorate_area_sqkm = units::set_units(st_area(.), km^2) %>%
-                        as.numeric()
-        ) %>%
-        relocate(
-                electorate,
-                electorate_area_sqkm,
-                geometry
-        ) %T>%
-        st_write(
-                dsn = "/QRISdata/Q4107/TS_electorates/clean_data/elect.clean.gpkg",
-                layer = "aus.clean", append = FALSE, delete_dsn = TRUE
-        )
+  select(
+    Elect_div, geometry
+  ) %>%
+  rename(
+    electorate = Elect_div
+  ) %>%
+  st_make_valid() %>%
+  st_crop(
+    xmin = 111.921114, ymin = -44.740510,
+    xmax = 154.638727, ymax = -8.115517
+    # remove the islands
+  ) %>%
+  mutate(
+    electorate_area_sqkm = units::set_units(st_area(.), km^2) %>%
+      as.numeric()
+  ) %>%
+  relocate(
+    electorate,
+    electorate_area_sqkm,
+    geometry
+  ) %T>%
+  st_write(
+    dsn = "/QRISdata/Q4107/TS_electorates/clean_data/elect.clean.gpkg",
+    layer = "elect.clean", append = FALSE, delete_dsn = TRUE
+  )
 
 elect.union.clean <- elect.clean %>%
-        st_union(by_feature = FALSE) %>%
-        st_sf() %>%
-        st_make_valid() %T>%
-        st_write(
-                dsn = "/QRISdata/Q4107/TS_electorates/clean_data/elect.union.clean.gpkg",
-                layer = "elect.union.clean", append = FALSE, delete_dsn = TRUE
-        )
+  st_union(by_feature = FALSE) %>%
+  st_sf() %>%
+  st_make_valid() %T>%
+  st_write(
+    dsn = "/QRISdata/Q4107/TS_electorates/clean_data/elect.union.clean.gpkg",
+    layer = "elect.union.clean", append = FALSE, delete_dsn = TRUE
+  )
 
 #### Import & clean: Species ####
 
 species <- st_read("/QRISdata/Q4107/TS_electorates/raw_data/SNES_public_1july2021.gdb")
-species.clean <- species %>%
-        filter(PRESENCE_RANK == 2) %>%
-        filter(!is.na(THREATENED_STATUS)) %>%
-        filter(THREATENED_STATUS %in% c(
-                "Vulnerable",
-                "Endangered",
-                "Critically Endangered"
-        )) %>% # as in wardNationalscaleDatasetThreats2021
-        filter(!MARINE %in% "Listed") %>%
-        filter(!SCIENTIFIC_NAME %in% c(
-                "Brachionichthys hirsutus", # Spotted Handfish
-                "Brachiopsilus ziebelli", # Ziebell's Handfish, Waterfall Bay Handfish
-                "Carcharias taurus (east coast population)", # Grey Nurse Shark (east coast population)
-                "Carcharias taurus (west coast population)", # Grey Nurse Shark (west coast population)
-                "Carcharodon carcharias", # White Shark, Great White Shark
-                "Epinephelus daemelii", # Black Rockcod, Black Cod, Saddled Rockcod
-                "Glyphis garricki", # Northern River Shark, New Guinea River Shark
-                "Glyphis glyphis", # Speartooth Shark
-                "Pristis clavata", # Dwarf Sawfish, Queensland Sawfish
-                "Rhincodon typus", # Whale Shark
-                "Thymichthys politus", # Red Handfish
-                "Zearaja maugeana" # Maugean Skate, Port Davey Skate
-        )) %>%
-        # some marine species were not being filtered out, remove all marine species, keep inland freshwater species
-        filter(!SCIENTIFIC_NAME %in% c(
-                "Calidris canutus", # Red Knot, Knot
-                "Calidris ferruginea", # Curlew Sandpiper
-                "Calidris tenuirostris", # Great Knot
-                "Hirundapus caudacutus" # White-throated Needletail
-        )) %>%
-        # MARINE == "Listed - overfly marine area"
-        filter(!CETACEAN %in% "Cetacean") %>%
-        select(c(
-                "SCIENTIFIC_NAME", "VERNACULAR_NAME", "THREATENED_STATUS",
-                "MIGRATORY_STATUS", "TAXON_GROUP", "Shape"
-        )) %>%
-        rename(
-                scientific_name = SCIENTIFIC_NAME,
-                vernacular_name = VERNACULAR_NAME,
-                threatened_status = THREATENED_STATUS,
-                migratory_status = MIGRATORY_STATUS,
-                taxon_group = TAXON_GROUP,
-                geometry = Shape
-        ) %>%
-        st_make_valid() %>%
-        # Merge species at the broad taxa as there are duplicate polygons
-        # Probably attributable to subspecies populations, but still ¯\_(ツ)_/¯
-        group_by(
-                scientific_name,
-                vernacular_name,
-                threatened_status,
-                migratory_status,
-                taxon_group
-        ) %>%
-        summarise() %>%
-        ungroup() %>%
-        st_make_valid() %>%
-        mutate(
-                species_range_area_sqkm = units::set_units(st_area(.), km^2) %>%
-                        as.numeric()
-        ) %>%
-        relocate(
-                scientific_name, vernacular_name,
-                threatened_status, migratory_status,
-                taxon_group, species_range_area_sqkm, geometry
-        ) %T>%
-        st_write(
-                dsn = "/QRISdata/Q4107/TS_electorates/clean_data/species.clean.gpkg",
-                layer = "species.clean", append = FALSE, delete_dsn = TRUE
-        )
+
+species.first.clean <- species %>%
+  filter(PRESENCE_RANK == 2) %>%
+  filter(!is.na(THREATENED_STATUS)) %>%
+  filter(THREATENED_STATUS %in% c(
+    "Vulnerable",
+    "Endangered",
+    "Critically Endangered"
+  )) %>% # as in wardNationalscaleDatasetThreats2021
+  filter(!MARINE %in% "Listed") %>%
+  filter(!SCIENTIFIC_NAME %in% c(
+    "Brachionichthys hirsutus", # Spotted Handfish
+    "Brachiopsilus ziebelli", # Ziebell's Handfish, Waterfall Bay Handfish
+    "Carcharias taurus (east coast population)", # Grey Nurse Shark (east coast population)
+    "Carcharias taurus (west coast population)", # Grey Nurse Shark (west coast population)
+    "Carcharodon carcharias", # White Shark, Great White Shark
+    "Epinephelus daemelii", # Black Rockcod, Black Cod, Saddled Rockcod
+    "Glyphis garricki", # Northern River Shark, New Guinea River Shark
+    "Glyphis glyphis", # Speartooth Shark
+    "Pristis clavata", # Dwarf Sawfish, Queensland Sawfish
+    "Rhincodon typus", # Whale Shark
+    "Thymichthys politus", # Red Handfish
+    "Zearaja maugeana" # Maugean Skate, Port Davey Skate
+  )) %>%
+  # some marine species were not being filtered out, remove all marine species, keep inland freshwater species
+  filter(!SCIENTIFIC_NAME %in% c(
+    "Calidris canutus", # Red Knot, Knot
+    "Calidris ferruginea", # Curlew Sandpiper
+    "Calidris tenuirostris", # Great Knot
+    "Hirundapus caudacutus" # White-throated Needletail
+  )) %>%
+  # MARINE == "Listed - overfly marine area"
+  filter(!CETACEAN %in% "Cetacean") %>%
+  select(c(
+    "SCIENTIFIC_NAME", "VERNACULAR_NAME", "THREATENED_STATUS",
+    "MIGRATORY_STATUS", "TAXON_GROUP", "Shape"
+  )) %>%
+  rename(
+    scientific_name = SCIENTIFIC_NAME,
+    vernacular_name = VERNACULAR_NAME,
+    threatened_status = THREATENED_STATUS,
+    migratory_status = MIGRATORY_STATUS,
+    taxon_group = TAXON_GROUP,
+    geometry = Shape
+  ) %>%
+  st_make_valid() %>%
+  # Merge species at the broad taxa as there are duplicate polygons
+  # Probably attributable to subspecies populations, but still ¯\_(ツ)_/¯
+  group_by(
+    scientific_name,
+    vernacular_name,
+    threatened_status,
+    migratory_status,
+    taxon_group
+  ) %>%
+  summarise() %>%
+  ungroup() %>%
+  st_make_valid() %>%
+  mutate(
+    species_range_area_sqkm = units::set_units(st_area(.), km^2) %>%
+      as.numeric()
+  ) %>%
+  relocate(
+    scientific_name, vernacular_name,
+    threatened_status, migratory_status,
+    taxon_group, species_range_area_sqkm, geometry
+  )
+
+species.edge.clean <- species.first.clean %>%
+  filter(
+    !scientific_name %in% c(
+      "Fregetta grallaria grallaria",
+      "Diomedea antipodensis gibsoni",
+      "Sternula nereis nereis",
+      "Thalassarche bulleri platei",
+      "Pachyptila turtur subantarctica",
+      "Pristis pristis",
+      "Pristis zijsron",
+      "Neophema chrysogaster",
+      "Pterodroma heraldica"
+    )
+  ) %>%
+  st_intersection(
+    elect.union.clean
+  ) %>%
+  mutate(
+    species_range_edge_clean_area_sqkm = units::set_units(st_area(.), km^2) %>%
+      as.numeric()
+  )
+
+species.edge.clean.not <- species.first.clean %>%
+  filter(
+    scientific_name %in% c(
+      "Fregetta grallaria grallaria",
+      "Diomedea antipodensis gibsoni",
+      "Sternula nereis nereis",
+      "Thalassarche bulleri platei",
+      "Pachyptila turtur subantarctica",
+      "Pristis pristis",
+      "Pristis zijsron",
+      "Neophema chrysogaster",
+      "Pterodroma heraldica"
+    )
+  )
+
+species.second.clean <- species.edge.clean %>%
+  bind_rows(species.edge.clean.not) %T>%
+  st_write(
+    dsn = "/QRISdata/Q4107/TS_electorates/clean_data/species.clean.gpkg",
+    layer = "species.clean", append = FALSE, delete_dsn = TRUE
+  )
 
 #### Import & clean: Demography ####
 
 demo <- readxl::read_xlsx(
-        "/QRISdata/Q4107/TS_electorates/raw_data/demographic-classification-as-at-2-august-2021.xlsx"
+  "/QRISdata/Q4107/TS_electorates/raw_data/demographic-classification-as-at-2-august-2021.xlsx"
 )
 
-demo <- demo %>%
-        rename(
-                state_territory = "State or territory",
-                demographic_class = "Demographic classification",
-                electorate = "Electoral division"
-        ) %>%
-        mutate(
-                state_territory = replace(
-                        state_territory, state_territory == "ACT", "Australian Capital Territory"
-                )
-        ) %>%
-        mutate(
-                state_territory = replace(
-                        state_territory, state_territory == "NT", "Northern Territory"
-                )
-        ) %>%
-        mutate(
-                state_territory_abbrev = case_when(
-                        state_territory == "Australian Capital Territory" ~ "ACT",
-                        state_territory == "New South Wales" ~ "NSW",
-                        state_territory == "Northern Territory" ~ "NT",
-                        state_territory == "Queensland" ~ "QLD",
-                        state_territory == "South Australia" ~ "SA",
-                        state_territory == "Tasmania" ~ "TAS",
-                        state_territory == "Victoria" ~ "VIC",
-                        state_territory == "Western Australia" ~ "WA"
-                )
-        ) %>%
-        mutate(
-                demographic_class = str_to_sentence(demographic_class)
-        ) %>%
-        mutate(
-                electorate_abbrev = abbreviate(
-                        electorate,
-                        minlength = 2L
-                )
-        ) %>%
-        mutate(
-                electorate_abbrev = replace(
-                        electorate_abbrev, electorate_abbrev == "E-", "E-M"
-                )
-        ) %>%
-        mutate(
-        electorate_abbrev = replace(
-                electorate_abbrev, electorate_abbrev == "O'", "O'C"
-        )
-        ) %>%
-        relocate(
-                electorate_abbrev,
-                .after = electorate
-        ) %T>%
-        write.csv(
-                "/QRISdata/Q4107/TS_electorates/clean_data/demo.clean.csv",
-                row.names = FALSE
-        )
+demo.clean <- demo %>%
+  rename(
+    state_territory = "State or territory",
+    demographic_class = "Demographic classification",
+    electorate = "Electoral division"
+  ) %>%
+  mutate(
+    state_territory = replace(
+      state_territory, state_territory == "ACT", "Australian Capital Territory"
+    )
+  ) %>%
+  mutate(
+    state_territory = replace(
+      state_territory, state_territory == "NT", "Northern Territory"
+    )
+  ) %>%
+  mutate(
+    state_territory_abbrev = case_when(
+      state_territory == "Australian Capital Territory" ~ "ACT",
+      state_territory == "New South Wales" ~ "NSW",
+      state_territory == "Northern Territory" ~ "NT",
+      state_territory == "Queensland" ~ "QLD",
+      state_territory == "South Australia" ~ "SA",
+      state_territory == "Tasmania" ~ "TAS",
+      state_territory == "Victoria" ~ "VIC",
+      state_territory == "Western Australia" ~ "WA"
+    )
+  ) %>%
+  mutate(
+    demographic_class = str_to_sentence(demographic_class)
+  ) %>%
+  mutate(
+    electorate_abbrev = abbreviate(
+      electorate,
+      minlength = 2L
+    )
+  ) %>%
+  mutate(
+    electorate_abbrev = replace(
+      electorate_abbrev, electorate_abbrev == "E-", "E-M"
+    )
+  ) %>%
+  mutate(
+    electorate_abbrev = replace(
+      electorate_abbrev, electorate_abbrev == "O'", "O'C"
+    )
+  ) %>%
+  relocate(
+    electorate_abbrev,
+    .after = electorate
+  ) %T>%
+  write.csv(
+    "/QRISdata/Q4107/TS_electorates/clean_data/demo.clean.csv",
+    row.names = FALSE
+  )
 
 #### Import & clean: States ####
 
-state.clean <- elect %>%
-        full_join(demo) %>%
-        group_by(
-                state_territory, state_territory_abbrev
-        ) %>%
-        summarise() %T>%
-        st_write(
-                dsn = "/QRISdata/Q4107/TS_electorates/clean_data/state.clean.gpkg",
-                layer = "state.clean", append = FALSE, delete_dsn = TRUE
-        )
+state.clean <- elect.clean %>%
+  full_join(demo.clean) %>%
+  group_by(
+    state_territory, state_territory_abbrev
+  ) %>%
+  summarise() %T>%
+  st_write(
+    dsn = "/QRISdata/Q4107/TS_electorates/clean_data/state.clean.gpkg",
+    layer = "state.clean", append = FALSE, delete_dsn = TRUE
+  )
