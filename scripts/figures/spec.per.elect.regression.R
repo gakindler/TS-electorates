862eec8b013e38d4ce119fed292a5524235dfd9d
gareth_kindler_lenovo_pop_os <42635017+gakindler@users.noreply.github.com>
2022-06-26 09:00:10 +1000

    pre-fixing lms and amalgamating

--- scripts/figures/spec.per.elect.regression.R
+++ scripts/figures/spec.per.elect.regression.R
@@ -12,36 +12,20 @@ library(plyr)
 library(dplyr)
 library(jsonlite)
 library(broom)
-
-#### Functions #### As per Chang 2018
-
-predictvals <- function(model, xvar, yvar, xrange = NULL, samples = 100, ...) {
-    # If xrange isn't passed in, determine xrange from the models.
-    # Different ways of extracting the x range, depending on model type
-    if (is.null(xrange)) {
-        if (any(class(model) %in% c("lm", "glm"))) {
-            xrange <- range(model$model[[xvar]])
-        } else if (any(class(model) %in% "loess")) {
-            xrange <- range(model$x)
-        }
-    }
-
-    newdata <- data.frame(x = seq(xrange[1], xrange[2], length.out = samples))
-    names(newdata) <- xvar
-    newdata[[yvar]] <- predict(model, newdata = newdata, ...)
-    newdata
-}
-
-make_model <- function(data) {
-    lm(total_unique_species ~ electorate_area_sqkm, data)
-}
+library(ggfortify)
+library(sars)
 
 #### Import ####
 
 spec.per.elect.counts.summary <- st_read(
     "analysed_data/local_analysis_output/spec.per.elect.counts.summary.gpkg"
 ) %>%
-    st_set_geometry(NULL)
+    st_set_geometry(NULL) %>%
+    select(
+        electorate, state_territory,
+        demographic_class, electorate_area_sqkm,
+        total_unique_species
+    )
 
 # spec.range.elect.expanded.summary <- read.csv(
 #   "analysed_data/local_analysis_output/spec.range.elect.expanded.summary.csv"
@@ -51,88 +35,67 @@ spec.per.elect.counts.summary <- st_read(
 #   "analysed_data/local_analysis_output/spec.per.elect.expanded.summary.csv"
 # )
 
-#### Sub-setting ####
+#### Linear regression ####
 
 spec.per.elect.IM <- spec.per.elect.counts.summary %>%
     filter(
         demographic_class %in% "Inner metropolitan"
-    )
+    ) %>%
+    mutate(log_electorate_area_sqkm = log(electorate_area_sqkm))
+hist(spec.per.elect.IM$electorate_area_sqkm)
+plot(spec.per.elect.IM$electorate_area_sqkm), spec.per.elect.IM$total_unique_species)
+boxplot(spec.per.elect.IM$electorate_area_sqkm, horizontal=TRUE)
+rug(jitter(log(spec.per.elect.IM$electorate_area_sqkm)), side=1)
+plot(total_unique_species~log(electorate_area_sqkm), spec.per.elect.IM)
+with(spec.per.elect.IM, lines(lowess(total_unique_species~log(electorate_area_sqkm))))
+
+IM.lm <- lm(total_unique_species ~ log(electorate_area_sqkm), data = spec.per.elect.IM)
+
+# https://b-rodrigues.github.io/modern_R/statistical-models.html#diagnostics
+autoplot(IM.lm, which = 1:6) + theme_minimal()
+
+# https://www.flutterbys.com.au/stats/tut/tut10.4.html
+p <- ecdf(log(spec.per.elect.counts.summary$electorate_area_sqkm))
+plot(p)
+
 
 spec.per.elect.OM <- spec.per.elect.counts.summary %>%
     filter(
         demographic_class %in% "Outer metropolitan"
     )
+hist(log(spec.per.elect.OM$electorate_area_sqkm))
+plot(log(spec.per.elect.OM$electorate_area_sqkm), spec.per.elect.OM$total_unique_species)
+shapiro.test(log(spec.per.elect.OM$electorate_area_sqkm))
 
 spec.per.elect.P <- spec.per.elect.counts.summary %>%
     filter(
         demographic_class %in% "Provincial"
     )
+hist(log(spec.per.elect.P$electorate_area_sqkm))
+plot(log(spec.per.elect.P$electorate_area_sqkm), spec.per.elect.P$total_unique_species)
+shapiro.test(log(spec.per.elect.P$electorate_area_sqkm))
 
 spec.per.elect.R <- spec.per.elect.counts.summary %>%
     filter(
         demographic_class %in% "Rural"
     )
+hist(log(spec.per.elect.R$electorate_area_sqkm))
+plot(log(spec.per.elect.R$electorate_area_sqkm), spec.per.elect.R$total_unique_species)
 
-#### Regression model ####
+boxplot(log(spec.per.elect.counts.summary$electorate_area_sqkm), horizontal=TRUE)
+rug(jitter(log(spec.per.elect.counts.summary$electorate_area_sqkm)), side=1)
 
-# LM, Gaussian, log10
-# Even discrete responses (such as counts that can only logically be positive integers) can occasionally be approximately described by a Gaussian distribution, particularly if either the samples are very large and the values free from boundary conditions (such as being close to a lower limit of 0), or else we are dealing with average counts.
-
-# https://towardsdatascience.com/poisson-regression-and-generalised-linear-models-606fe5f7c1fd
-# Do you think Linear Regression would be a suitable model? The answer is NO for the following reasons:
-# The number of calls have to be greater or equal to 0, whereas in Linear Regression the output can be negative as well as positive.
-# The number of calls only take integer values but Linear Regression can output fractional values.
-
-lm_IM <- lm(
-    total_unique_species ~ log(electorate_area_sqkm),
-    data = spec.per.elect.IM
-)
-lm_OM <- lm(
-    total_unique_species ~ electorate_area_sqkm,
-    data = spec.per.elect.OM
-)
-lm_P <- lm(
-    total_unique_species ~ electorate_area_sqkm,
-    data = spec.per.elect.P
-)
-lm_R <- lm(
-    total_unique_species ~ electorate_area_sqkm,
-    data = spec.per.elect.R
-)
-
-broom::glance(lm_IM)
-broom::glance(lm_OM)
-broom::glance(lm_P)
-broom::glance(lm_R)
-broom::tidy(lm_IM)
-broom::tidy(lm_OM)
-broom::tidy(lm_P)
-broom::tidy(lm_R)
-
-
-
-xmin <- min(log(spec.per.elect.IM$electorate_area_sqkm))
-xmax <- max(log(spec.per.elect.IM$electorate_area_sqkm))
-
-predicted <- data.frame(electorate_area_sqkm = seq(xmin, xmax, length.out = 100))
-
-predicted$total_unique_species <- predict(lm_IM, predicted)
-
-# https://stats.stackexchange.com/questions/477598/equivalent-of-r-squared-in-generalized-linear-model-regression-results
-
-# https://stats.stackexchange.com/questions/298/in-linear-regression-when-is-it-appropriate-to-use-the-log-of-an-independent-va
-
-# TODO: getting coefficients etc into paper - https://data.library.virginia.edu/interpreting-log-transformations-in-a-linear-model/
-# https://www.statology.org/interpret-glm-output-in-r/
+plot(total_unique_species~log(electorate_area_sqkm), spec.per.elect.counts.summary)
+with(spec.per.elect.counts.summary, lines(lowess(total_unique_species~log(electorate_area_sqkm))))
 
 
 lm_labels <- function(dat) {
-    mod <- lm(total_unique_species ~ log(electorate_area_sqkm), data = dat)
+    mod <- lm(total_unique_species ~ log2(electorate_area_sqkm), data = dat)
     formula <- sprintf(
         "italic(y) == %.2f %+.2f * italic(x)",
         round(coef(mod)[1], 2), round(coef(mod)[2], 2)
     )
-    r <- cor(log(dat$electorate_area_sqkm), dat$total_unique_species)
+    r <- cor(log2(dat$electorate_area_sqkm), dat$total_unique_species)
     r2 <- sprintf("italic(R^2) == %.2f", r^2)
     data.frame(formula = formula, r2 = r2, stringsAsFactors = FALSE)
 }
@@ -141,14 +104,8 @@ labels <- spec.per.elect.counts.summary %>%
     group_by(demographic_class) %>%
     do(lm_labels(.))
 
-
-#### Scatter plot with regression line ####
-
-spec.per.elect.IM$electorate_area_sqkm <- log(spec.per.elect.IM$electorate_area_sqkm)
-
 # spec.per.elect.lm.facet <-
-    # ggplot(spec.per.elect.counts.summary) +
-    ggplot(spec.per.elect.IM) +
+    ggplot(spec.per.elect.counts.summary) +
     aes(
         x = electorate_area_sqkm,
         y = total_unique_species
@@ -156,19 +113,19 @@ spec.per.elect.IM$electorate_area_sqkm <- log(spec.per.elect.IM$electorate_area_
     geom_point(
         alpha = 0.4
     ) +
-    # geom_smooth(
-    #     method = "lm",
-    #     show.legend = FALSE,
-    #     colour = "black",
-    #     se = FALSE
-    #     # method.args = list(
-    #     #     family = quasipoisson()
-    #     # )
-    # ) +
-    geom_line(
-        data = predicted,
-        size = 1
+    geom_smooth(
+        # method = "gam",
+        show.legend = FALSE,
+        colour = "black",
+        se = FALSE
+        # method.args = list(
+        #     family = quasipoisson()
+        # )
     )
+    # geom_line(
+    #     data = predicted,
+    #     size = 1
+    # )
     # scale_x_continuous(
         # trans = "log",
         # labels = scales::comma,
@@ -178,7 +135,7 @@ spec.per.elect.IM$electorate_area_sqkm <- log(spec.per.elect.IM$electorate_area_
         # limits = c(0, 280)
     # ) +
     labs(
-        x = bquote(Log ~ "electorate area" ~ (km^2)),
+        x = bquote(Log[2] ~ "Commonwealth Electoral Division area" ~ (km^2)),
         y = "Threatened species"
     ) +
     # guides(
@@ -187,15 +144,15 @@ spec.per.elect.IM$electorate_area_sqkm <- log(spec.per.elect.IM$electorate_area_
     #         override.aes = list(size = 3)
     #     )
     # ) +
-    theme_bw()
+    theme_bw() +
     facet_wrap(~demographic_class) +
     geom_text(
         data = labels, aes(
             label = formula
-        ), x = 5, y = 200, parse = TRUE, hjust = 0
+        ), x = 5.2, y = 205, parse = TRUE, hjust = 0
     ) +
     geom_text(
-        x = 5, y = 180, aes(
+        x = 5.2, y = 180, aes(
             label = r2
         ), data = labels, parse = TRUE, hjust = 0
     ) +
@@ -204,7 +161,7 @@ spec.per.elect.IM$electorate_area_sqkm <- log(spec.per.elect.IM$electorate_area_
     )
 
 ggsave("figures/spec.per.elect.lm.facet.png",
-    spec.per.elect.lm.facet,
+    # spec.per.elect.lm.facet,
     width = 20, height = 15, units = "cm"
 )
 
@@ -219,6 +176,41 @@ ggplot(spec.per.elect.counts.summary) +
         show.legend = FALSE,
         colour = "black"
     )
+
+#### GLM ####
+
+#### sars R package ####
+
+data(galap)
+fit <- sar_loga(data = galap, grid_start = "partial")
+summary(fit)
+plot(fit)
+
+sars_spec_per_elects <- spec.per.elect.counts.summary %>%
+select(electorate_area_sqkm, total_unique_species)
+
+fit_loga <- sar_loga(sars_spec_per_elects)
+plot(fit_loga)
+
+fit_C <- sar_multi(sars_spec_per_elects, obj = c("power", "loga", "monod"))
+plot(fit_C)
+
+fit_lm <- sar_linear(sars_spec_per_elects, normaTest ="shapiro", homoTest = "cor.fitted")
+summary(fit_lm)
+
+fit_av <- sar_average(
+    data = sars_spec_per_elects,
+    normaTest = "shapiro",
+    homoTest = "cor.fitted"
+)
+
+fit_lm_log <- lin_pow(
+    data = sars_spec_per_elects,
+    compare = TRUE,
+    normaTest = "shapiro"
+    )
+summary(fit_lm_log)
+plot(fit_lm_log)
 
 #### Calculations ####
 #
